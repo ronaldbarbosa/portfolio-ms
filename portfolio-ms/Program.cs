7487c98fcc6809fc0af2f811a48281fadbe80eee
Ronald Barbosa <ronaldverissimo@hotmail.com>
2025-04-26 17:19:55 -0300

    feat: configured CORS.

--- portfolio-ms/Program.cs
+++ portfolio-ms/Program.cs
@@ -15,6 +15,16 @@ builder.Services.AddDbContext<AppDbContext>(options =>
 
 builder.Services.AddTransient<IProjectHandler, ProjectHandler>();
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll", policy =>
+    {
+        policy.AllowAnyOrigin()
+            .AllowAnyHeader()
+            .AllowAnyMethod();
+    });
+});
+
 var app = builder.Build();
 
 app.MapOpenApi();
@@ -26,5 +36,7 @@ app.MapScalarApiReference(options =>
 });
 app.MapEndpoints();
 app.UseHttpsRedirection();
+
+app.UseCors("AllowAll");
 
 app.Run();
